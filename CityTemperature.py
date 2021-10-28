be2a4876856a2c5ce30847037d23cab2085c6dde
Josep <stoutxuzzo@gmail.com>
2021-10-28 17:46:19 +0200

    finished

--- CityTemperature.py
+++ CityTemperature.py
@@ -1,13 +1,12 @@
 import requests, json, urllib3
 
-#urllib3.disable_warnings()
+urllib3.disable_warnings()
 
 global apikey
 apikey = "Ftk2nOMhCvKGR10QPbmYs6cYjxgd83za"
 
 def getCityInfo(cityName):
     url = "http://dataservice.accuweather.com/locations/v1/cities/search"
-    
     response = requests.request("GET", url + "?apikey=" + apikey + "&q=" + cityName, verify = False)
     cityInfo = json.loads(response.content)
     return cityInfo[0]
@@ -18,21 +17,27 @@ def getCityWeather(cityKey):
     cityWeather = json.loads(response.content)
     return cityWeather[0]
 
-city = input("Input a city name: ")
-city = getCityInfo(city)
-key = city["Key"]
-weather = getCityWeather(key)
+while True:
+    user = input("Input a city name or 'exit' to close: ")
 
-print("\n" + city["EnglishName"] + ":")
-print("   Weather: " + weather["WeatherText"])
+    if user.upper() == "EXIT":
+        break
 
-if weather["HasPrecipitation"]:
-    print("   Precipitation: " + weather["PrecipitationType"])
-else:
-    print("   Precipitation: No")
+    city = getCityInfo(user)
+    key = city["Key"]
+    weather = getCityWeather(key)
 
-print("   Temperature: " + str(weather["Temperature"]["Metric"]["Value"]), end="")
-print(weather["Temperature"]["Metric"]["Unit"], end="")
-print("  /  " + str(weather["Temperature"]["Imperial"]["Value"]), end="")
-print(weather["Temperature"]["Imperial"]["Unit"])
+    print("\n" + city["EnglishName"] + ":")
+    print("   Weather: " + weather["WeatherText"])
+
+    if weather["HasPrecipitation"]:
+        print("   Precipitation: " + weather["PrecipitationType"])
+    else:
+        print("   Precipitation: No")
+
+    print("   Temperature: " + str(weather["Temperature"]["Metric"]["Value"]), end="")
+    print(weather["Temperature"]["Metric"]["Unit"], end="")
+    print("  /  " + str(weather["Temperature"]["Imperial"]["Value"]), end="")
+    print(weather["Temperature"]["Imperial"]["Unit"])
+    print("")
 
